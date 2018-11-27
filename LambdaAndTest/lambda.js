91d5a91b896d7d6e307b879d7e39c9a47916c9e9
sigmaidetest <cehib@balanc3r.com>
2018-11-27 16:05:44 +0530

    init

---
+++ LambdaAndTest/lambda.js
@@ -0,0 +1,16 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function (event, context, callback) {
+
+    ddb.put({
+        TableName: 'LambdaAndDDB',
+        Item: { 'pKey': 1, 'pName': 'test' }
+    }).promise().then(function (data) {
+        //your logic goes here
+    }).catch(function (err) {
+        //handle error
+    });
+
+    callback(null, { "message": "Successfully executed" });
+}
