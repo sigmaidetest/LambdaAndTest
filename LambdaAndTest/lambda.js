63ac76cfe8e3b1daae697a1e9fa7ceea43f2df1b
sigmaidetest <cehib@balanc3r.com>
2018-11-27 16:12:03 +0530

    wd

--- LambdaAndTest/lambda.js
+++ LambdaAndTest/lambda.js
@@ -5,7 +5,11 @@ exports.handler = function (event, context, callback) {
 
     ddb.put({
         TableName: 'LambdaAndDDB',
-        Item: { 'pKey': 1, 'pName': 'test' }
+        Item: {
+            'pKey': 1,
+            'pName': 'test',
+            'pVersion': 1
+        }
     }).promise().then(function (data) {
         //your logic goes here
     }).catch(function (err) {
